007183ffaeaed197a224d2660ea9f9cc52080eba
siya186 <sakshi.goswami23@lpu.in>
2025-12-26 00:02:24 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/geoguessr-clone/', // Replace with your repo name
+  base: '/geo-guesser/',
   build: {
     outDir: 'dist',
     sourcemap: false,
